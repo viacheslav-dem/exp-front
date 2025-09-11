f2f0b74b8de11f714d6e0bc313858671e4b6fa8d
Morozov Dmitry <morozpegass@gmail.com>
2025-09-11 14:52:38 +0300

    council conclusion default form change by adding new fields

--- web/src/app/components/document-form/council-conclusion-8-3-4-5-7-8-12-15-form/council-conclusion-8-3-4-5-7-8-12-15-form.component.ts
+++ web/src/app/components/document-form/council-conclusion-8-3-4-5-7-8-12-15-form/council-conclusion-8-3-4-5-7-8-12-15-form.component.ts
@@ -19,4 +19,42 @@ export class CouncilConclusion_8_3_4_5_7_8_12_15_FormComponent extends CouncilCo
       throw 'Пожалуйста, заполните все поля заключения.';
     }
   }
+
+    showTarget8_1() {
+        let is8_1 = false;
+        // Использовал регулярные выражения, т.к. проверка возвращала true для 8.1 и 8.11, 8.12 и т.д.
+        const str = /8.1(.*)/;
+        const isDigit = /\d/
+        let code = this.project.code.code
+        if (code.startsWith('8.1')) {
+            let testReg1 = code.match(str);
+            if (testReg1 && testReg1[1]) {
+                is8_1 = !isDigit.test(testReg1[1]);
+            } else {
+                is8_1 = true;
+            }
+        }
+        return is8_1;
+    }
+
+    showTarget8_3() {
+        return this.project.code.code.startsWith('8.3');
+    }
+
+    showTarget8_4() {
+        return this.project.code.code.startsWith('8.4');
+    }
+
+
+    showTarget8_8() {
+        return this.project.code.code.startsWith('8.8');
+    }
+
+    showTarget8_2() {
+        return this.project.code.code.startsWith('8.2');
+    }
+
+    showTarget8_15() {
+        return this.project.code.code.startsWith('8.15');
+    }
 }
