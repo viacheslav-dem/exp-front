6a5821b4de6e8de547abf29d1fd0b4da4a6b420a
Pralkin <Pralkin@BELISA.ORG.BY>
2025-12-24 12:42:17 +0300

    Добавлены условия до 5000 символов в форме заключение эксперта 13

--- web/src/app/components/document-form/expert-review-8-13-2025-form/expert-review_8_13_2025-form.component.ts
+++ web/src/app/components/document-form/expert-review-8-13-2025-form/expert-review_8_13_2025-form.component.ts
@@ -6,9 +6,8 @@ import {
 } from "@app/components/document-form/form-model/ExpertReview_8_13_2025FormContent";
 
 @Component({
-    selector: 'app-review-8-13-2025-form',
-    templateUrl: './expert-review_8_13_2025-form.component.html',
-    standalone: false
+  selector: 'app-review-8-13-2025-form',
+  templateUrl: './expert-review_8_13_2025-form.component.html'
 })
 export class ExpertReview_8_13_2025FormComponent extends ExpertReviewForm<ExpertReview_8_13_2025FormContent> {
 
@@ -18,10 +17,11 @@ export class ExpertReview_8_13_2025FormComponent extends ExpertReviewForm<Expert
         && !this._form.prognosis
         && !this._form.targetAnalysis
         && !this._form.programSufficiency
-       // || this._form.selectedDirections.length + this._form.selectedSocialEconomicGoals.length == 0;
+    // || this._form.selectedDirections.length + this._form.selectedSocialEconomicGoals.length == 0;
     if (disabled) {
       this._form.conclusion = false;
     }
+    console.log(disabled);
     return disabled;
   }
 
@@ -63,23 +63,23 @@ export class ExpertReview_8_13_2025FormComponent extends ExpertReviewForm<Expert
   }
 
   private validationLengthCommentsOnConclusionAnalysisAndEvaluation() {
-    if (this._form.priorityAreasText.length < 30) {
-      throw "В пункте 'Соответствие приоритетным направлениям научной, научно-технической и инновационной деятельности в Республике Беларусь.' комментарий должен быть не менее 30 символов."
+    if (this._form.priorityAreasText.length < 30 || this._form.priorityAreasText.length > 5000) {
+      throw "В пункте 'Соответствие приоритетным направлениям научной, научно-технической и инновационной деятельности в Республике Беларусь.' комментарий должен быть не менее 30 символов и не более 5000 символов."
     }
-    if (this._form.programRequirementsText.length < 30) {
-      throw "В пункте '2. Соответствие требованиям, указанным в Положении о порядке формирования, финансирования, выполнения и оценки эффективности реализации государственных программ' комментарий должен быть не менее 30 символов."
+    if (this._form.programRequirementsText.length < 30 || this._form.programRequirementsText.length > 5000) {
+      throw "В пункте '2. Соответствие требованиям, указанным в Положении о порядке формирования, финансирования, выполнения и оценки эффективности реализации государственных программ' комментарий должен быть не менее 30 символов и не более 5000 символов."
     }
-    if (this._form.prognosisText.length < 30) {
-      throw "В пункте '3. Оценка анализа текущего состояния и прогноза научно-технического развития соответствующей сферы планирования.' комментарий должен быть не менее 30 символов."
+    if (this._form.prognosisText.length < 30 || this._form.prognosisText.length > 5000) {
+      throw "В пункте '3. Оценка анализа текущего состояния и прогноза научно-технического развития соответствующей сферы планирования.' комментарий должен быть не менее 30 символов и не более 5000 символов."
     }
-    if (this._form.targetAnalysisText.length < 30) {
-      throw "В пункте '4. Анализ целевых показателей:' комментарий должен быть не менее 30 символов."
+    if (this._form.targetAnalysisText.length < 30 || this._form.targetAnalysisText.length > 5000) {
+      throw "В пункте '4. Анализ целевых показателей:' комментарий должен быть не менее 30 символов и не более 5000 символов."
     }
-    if (this._form.programSufficiencyText.length < 30) {
-      throw "В пункте '5. Достаточность перечня мероприятий по научному обеспечению государственной программы / перечня заданий государственной научно-технической программы для достижения запланированных программой целевых показателей:' комментарий должен быть не менее 30 символов."
+    if (this._form.programSufficiencyText.length < 30 || this._form.programSufficiencyText.length > 5000) {
+      throw "В пункте '5. Достаточность перечня мероприятий по научному обеспечению государственной программы / перечня заданий государственной научно-технической программы для достижения запланированных программой целевых показателей:' комментарий должен быть не менее 30 символов и не более 5000 символов."
     }
-    if (this._form.conclusionText.length < 30) {
-      throw "В пункте 'Заключение эксперта по объекту государственной экспертизы.' комментарий должен быть не менее 30 символов."
+    if (this._form.conclusionText.length < 30 || this._form.conclusionText.length > 5000) {
+      throw "В пункте 'Заключение эксперта по объекту государственной экспертизы.' комментарий должен быть не менее 30 символов и не более 5000 символов."
     }
   }
 }
