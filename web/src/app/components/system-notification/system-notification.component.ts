481291fbd6a958e5d568bfd6d192833a17155172
Viacheslav Kukhta <it@gknt.gov.by>
2026-02-27 12:57:40 +0300

    fix: background switch problem on system-notification system-notification.component.ts converted to signal

--- web/src/app/components/system-notification/system-notification.component.ts
+++ web/src/app/components/system-notification/system-notification.component.ts
@@ -1,10 +1,10 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from "@angular/core";
+import {ChangeDetectionStrategy, Component, computed, DestroyRef, inject, signal} from "@angular/core";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 import {DialogService} from "@app/components/dialogs/dialog.service";
 import {SystemNotificationService} from "@app/services/system-notification.service";
-import {SystemNotificationDto,} from "@app/dto/SystemNotificationDto";
-import {SafeHtmlPipe} from "@app/pipes/safe-html-pipe";
+import {SystemNotificationDto} from "@app/dto/SystemNotificationDto";
 import {environment} from "../../../environments/environment";
-import {finalize, take} from "rxjs";
+import {finalize, EMPTY, switchMap, take} from "rxjs";
 import {SystemNotificationStore} from "@app/services/system-notification.store";
 
 @Component({
@@ -15,69 +15,76 @@ import {SystemNotificationStore} from "@app/services/system-notification.store";
 })
 export class SystemNotificationComponent {
 
-    notification: SystemNotificationDto;
+    private notificationService = inject(SystemNotificationService);
+    private dialogService = inject(DialogService);
+    private notificationStore = inject(SystemNotificationStore);
+    private destroyRef = inject(DestroyRef);
 
-    notificationNames: string[];
-    selectedNotificationName = 'Оповещение не выбрано';
+    notification = signal<SystemNotificationDto>(new SystemNotificationDto());
 
-    notificationTypes: string[];
-    selectedNotificationType= 'Тип оповещения не выбран';
+    readonly notificationNames = getAllEnumNames(SystemNotificationNames);
+    readonly notificationTypes = getAllEnumNames(SystemNotificationTypes);
 
-    displayedNotificationStyleClass = "";
+    selectedNotificationName = signal('Оповещение не выбрано');
+    selectedNotificationType = signal('Тип оповещения не выбран');
 
-    isSaving = false;
+    displayedNotificationStyleClass = signal('');
 
+    isSaving = signal(false);
 
-    constructor(private notificationService: SystemNotificationService,
-                private dialogService: DialogService,
-                private safeHtmlPipe: SafeHtmlPipe,
-                private notificationStore: SystemNotificationStore,
-                private cdr: ChangeDetectorRef) {
-    }
+    readonly hasNotificationSelected = computed(() => this.selectedNotificationName() !== 'Оповещение не выбрано');
+    readonly hasMessage = computed(() => this.notification().message !== '');
 
     ngOnInit() {
-        this.notification = new SystemNotificationDto();
-        this.notification.message = "";
-        this.notificationNames = getAllEnumNames(SystemNotificationNames);
-        this.notificationTypes = getAllEnumNames(SystemNotificationTypes);
+        this.notification.set({ ...new SystemNotificationDto(), message: '' });
     }
 
-    public changeNotification(selectedName: string) {
+    changeNotification(selectedName: string) {
+        this.selectedNotificationName.set(selectedName);
+        const nameKey = Object.keys(SystemNotificationNames)[this.notificationNames.indexOf(selectedName)];
 
-        this.notification.name = Object.keys(SystemNotificationNames)[this.notificationNames.indexOf(selectedName)];
-
-        if (this.notification.name == null) {
-            this.notification.message = "";
-            this.cdr?.markForCheck?.();
-        } else {
-
-            this.notificationService.getNotification(this.notification).subscribe(
-                (response) => {
-                    this.notification = response;
-                    this.selectedNotificationType = SystemNotificationTypes[response.type];
-
-                    switch (this.selectedNotificationName) {
-                        case SystemNotificationNames.LOGIN_PAGE_NOTIFICATION:
-                            this.displayedNotificationStyleClass = "test-card-notification"; break;
-                        case SystemNotificationNames.ALL_PAGES_NOTIFICATION:
-                            this.displayedNotificationStyleClass = "card-notification-all-pages"; break;
-                    }
-                    this.cdr?.markForCheck?.();
-                },
-                () => {
-                    this.notification.message = "";
-                    this.cdr?.markForCheck?.();
-                });
+        if (nameKey == null) {
+            this.notification.update(n => ({ ...n, message: '' }));
+            return;
         }
+
+        const req = { ...this.notification(), name: nameKey };
+        this.notificationService.getNotification(req).pipe(
+            takeUntilDestroyed(this.destroyRef),
+        ).subscribe({
+            next: (response) => {
+                this.notification.set(response);
+                this.selectedNotificationType.set(SystemNotificationTypes[response.type] ?? SystemNotificationTypes.EMPTY_BACKGROUND);
+                const styleClass =
+                    selectedName === SystemNotificationNames.LOGIN_PAGE_NOTIFICATION
+                        ? 'test-card-notification'
+                        : selectedName === SystemNotificationNames.ALL_PAGES_NOTIFICATION
+                            ? 'card-notification-all-pages'
+                            : '';
+                this.displayedNotificationStyleClass.set(styleClass);
+            },
+            error: () => {
+                this.notification.update(n => ({ ...n, message: '' }));
+            },
+        });
     }
 
-    public changeNotificationType(selectedType: string) {
+    changeNotificationType(selectedType: string) {
+        this.selectedNotificationType.set(selectedType);
+        const typeKey = Object.keys(SystemNotificationTypes)[this.notificationTypes.indexOf(selectedType)];
+        this.notification.update(n => ({ ...n, type: typeKey }));
+    }
 
-        this.notification.type = Object.keys(SystemNotificationTypes)[this.notificationTypes.indexOf(selectedType)];
+    onEnabledChange(enabled: boolean) {
+        this.notification.update(n => ({ ...n, enabled }));
+    }
+
+    onMessageChange(message: string) {
+        this.notification.update(n => ({ ...n, message }));
     }
 
     postNotification(message: string) {
-        if (this.isSaving) {
+        if (this.isSaving()) {
             return;
         }
 
@@ -85,44 +92,38 @@ export class SystemNotificationComponent {
             'Сохранение системного уведомления',
             `Сохранить системное уведомление?`,
             'Пожалуйста, проверьте данные уведомления, поскольку предыдущее уведомление заменится новым.')
-            .pipe(take(1))
-            .subscribe(() => {
-                this.isSaving = true;
-                this.notification.message = message ?? "";
-                this.cdr?.markForCheck?.();
-
-                // Важно: reload должен быть ПОСЛЕ успешного ответа, иначе перезагрузка может оборвать HTTP-запрос.
-                this.notificationService.saveNotification(this.notification)
-                    .pipe(finalize(() => {
-                        this.isSaving = false;
-                        this.cdr?.markForCheck?.();
-                    }))
-                    .subscribe({
-                        next: (res) => {
-                            this.notification = res;
-                            // Обновляем превью-класс (на случай, если changeNotification не вызывался)
-                            if (res?.name === 'LOGIN_PAGE_NOTIFICATION') {
-                                this.displayedNotificationStyleClass = "test-card-notification";
-                            }
-                            if (res?.name === 'ALL_PAGES_NOTIFICATION') {
-                                this.displayedNotificationStyleClass = "card-notification-all-pages";
-                            }
-                            this.cdr?.markForCheck?.();
-                            // Обновляем глобальные баннеры без reload страницы
-                            this.notificationStore.applySaved(res);
-                            // Дополнительно перечитываем с сервера (на случай server-side нормализации/дефолтов)
-                            if (res?.name === 'ALL_PAGES_NOTIFICATION') {
-                                this.notificationStore.refreshAllPages();
-                            }
-                            if (res?.name === 'LOGIN_PAGE_NOTIFICATION') {
-                                this.notificationStore.refreshLoginPage();
-                            }
-                        },
-                        error: (err) => {
-                            // Ошибка уже может быть обработана глобальным HttpClientSecure, но лог оставим для диагностики.
-                            console.error('Failed to save system notification', err);
-                        }
-                    });
+            .pipe(
+                take(1),
+                switchMap(() => {
+                    this.isSaving.set(true);
+                    this.notification.update(n => ({ ...n, message: message ?? '' }));
+                    return this.notificationService.saveNotification(this.notification()).pipe(
+                        finalize(() => this.isSaving.set(false)),
+                    );
+                }),
+                takeUntilDestroyed(this.destroyRef),
+            )
+            .subscribe({
+                next: (res) => {
+                    this.notification.set(res);
+                    this.selectedNotificationType.set(SystemNotificationTypes[res?.type] ?? SystemNotificationTypes.EMPTY_BACKGROUND);
+                    if (res?.name === 'LOGIN_PAGE_NOTIFICATION') {
+                        this.displayedNotificationStyleClass.set('test-card-notification');
+                    }
+                    if (res?.name === 'ALL_PAGES_NOTIFICATION') {
+                        this.displayedNotificationStyleClass.set('card-notification-all-pages');
+                    }
+                    this.notificationStore.applySaved(res);
+                    if (res?.name === 'ALL_PAGES_NOTIFICATION') {
+                        this.notificationStore.refreshAllPages();
+                    }
+                    if (res?.name === 'LOGIN_PAGE_NOTIFICATION') {
+                        this.notificationStore.refreshLoginPage();
+                    }
+                },
+                error: (err) => {
+                    console.error('Failed to save system notification', err);
+                },
             });
     }
 
@@ -131,42 +132,39 @@ export class SystemNotificationComponent {
             'Удаление системного уведомления',
             `Удалить системное уведомление?`,
             'Уведомление будет удалено из базы данных.')
-            .subscribe(() => {
-
-                if (this.isSaving) {
-                    return;
-                }
-                this.isSaving = true;
-                this.notification.message = "";
-                this.notification.enabled = false;
-                this.notification.type = "EMPTY_BACKGROUND";
-
-                this.notificationService.saveNotification(this.notification)
-                    .pipe(finalize(() => {
-                        this.isSaving = false;
-                        this.cdr?.markForCheck?.();
-                    }))
-                    .subscribe({
-                        next: (res) => {
-                            this.notification = res;
-                            this.notificationStore.applySaved(res);
-                            if (res?.name === 'ALL_PAGES_NOTIFICATION') {
-                                this.notificationStore.refreshAllPages();
-                            }
-                            if (res?.name === 'LOGIN_PAGE_NOTIFICATION') {
-                                this.notificationStore.refreshLoginPage();
-                            }
-                            this.cdr?.markForCheck?.();
-                        },
-                        error: (err) => {
-                            console.error('Failed to delete system notification', err);
-                        }
-                    });
-            })
-    }
-
-    notificationToSafeHtml(notificationMessage: string) {
-        return this.safeHtmlPipe.transform(notificationMessage);
+            .pipe(
+                switchMap(() => {
+                    if (this.isSaving()) {
+                        return EMPTY;
+                    }
+                    this.isSaving.set(true);
+                    const payload = {
+                        ...this.notification(),
+                        message: '',
+                        enabled: false,
+                        type: 'EMPTY_BACKGROUND',
+                    };
+                    return this.notificationService.saveNotification(payload).pipe(
+                        finalize(() => this.isSaving.set(false)),
+                    );
+                }),
+                takeUntilDestroyed(this.destroyRef),
+            )
+            .subscribe({
+                next: (res) => {
+                    this.notification.set(res);
+                    this.notificationStore.applySaved(res);
+                    if (res?.name === 'ALL_PAGES_NOTIFICATION') {
+                        this.notificationStore.refreshAllPages();
+                    }
+                    if (res?.name === 'LOGIN_PAGE_NOTIFICATION') {
+                        this.notificationStore.refreshLoginPage();
+                    }
+                },
+                error: (err) => {
+                    console.error('Failed to delete system notification', err);
+                },
+            });
     }
 }
 
@@ -182,7 +180,6 @@ export enum SystemNotificationTypes {
     IMPORTANT_BACKGROUND = 'Важное сообщение (красный фон)'
 }
 
-export function getAllEnumNames(enumType: any) {
-
+export function getAllEnumNames(enumType: Record<string, string>) {
     return Object.keys(enumType).map((key) => enumType[key]);
 }
